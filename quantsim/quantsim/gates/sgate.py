73583ea53d8f42dba94abc2afddb90c60cb06091
leanashaji <leanashaji0@gmail.com>
2026-02-04 11:28:50 +0530

    feat: added SGate

---
+++ quantsim/quantsim/gates/sgate.py
@@ -0,0 +1,8 @@
+import numpy as np
+from .gate import QuantumGate
+
+
+class SGate(QuantumGate):
+    def __init__(self):
+        mat = np.matrix([[1, 0],[0, 1j]], np.complex128)
+        super().__init__(mat, "S")
