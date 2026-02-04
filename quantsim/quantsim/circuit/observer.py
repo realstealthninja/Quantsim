1e7bfcd4923d226ebd715d18e6c0692a038f34b8
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-04 08:42:31 +0530

    fix: add value property

--- quantsim/quantsim/circuit/observer.py
+++ quantsim/quantsim/circuit/observer.py
@@ -2,6 +2,8 @@ from .component import Component
 
 
 class Observer(Component):
+    value: bool
+    
     def __init__(self, inputs: list[Component]) -> None:
         super().__init__(inputs, None)
 
