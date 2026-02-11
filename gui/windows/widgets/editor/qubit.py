b4282a9d39cfec1e89acbff8c51a2afbd6afff03
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-11 12:02:15 +0530

    fix: add qubit to qubitcaditem

--- gui/windows/widgets/editor/qubit.py
+++ gui/windows/widgets/editor/qubit.py
@@ -1,7 +1,11 @@
-class QubitCADItem(CADItem):
-    id: int
+from PySide6.QtCore import QPointF
+from quantsim.core.qubit import Qubit
+from .caditem import CADItem
 
-    def __init__(self):
-        super().__init__("./assets/qubit.svg")
-        
-    
+
+class QubitCADItem(CADItem):
+    qubit: Qubit
+
+    def __init__(self, qubit: Qubit | None = None):
+        super().__init__("./assets/qubit.svg", None, QPointF(490/512, 260/512))
+        self.qubit = qubit if qubit else Qubit()
