0b735278504547f2fd36e0990ab08a899271e79c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-11 12:01:43 +0530

    fix: add import s and input/output positions

--- gui/windows/widgets/editor/caditem.py
+++ gui/windows/widgets/editor/caditem.py
@@ -1,5 +1,15 @@
+from PySide6.QtCore import QPointF, QSize
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QGraphicsPixmapItem
+
+
 class CADItem(QGraphicsPixmapItem):
-    pixmap: QPixmap
-    def __init__(self, pixmap: QPixmap):
-        self.pixmap = pixmap
-        super().__init__(pixmap)
+    input_position: QPointF | None
+    output_position: QPointF | None
+
+    def __init__(self, path: str, input_position: QPointF | None, output_postion: QPointF | None) -> None:
+        super().__init__(QPixmap(path).scaled(QSize(64, 64)))
+        self.input_position = input_position
+        self.output_position = output_postion
+
+    
