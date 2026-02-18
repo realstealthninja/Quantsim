504b6f0ae144da9cc78df865456684dc2e635af6
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-18 07:36:56 +0530

    chore: override hoverevent

--- gui/windows/widgets/editor/qubit.py
+++ gui/windows/widgets/editor/qubit.py
@@ -1,4 +1,6 @@
+from typing import override
 from PySide6.QtCore import QPointF
+from PySide6.QtWidgets import QGraphicsSceneHoverEvent
 from quantsim.core.qubit import Qubit
 from .caditem import CADItem
 
@@ -9,3 +11,10 @@ class QubitCADItem(CADItem):
     def __init__(self, qubit: Qubit | None = None):
         super().__init__("./assets/qubit.svg", None, QPointF(490/512, 260/512))
         self.qubit = qubit if qubit else Qubit()
+<<<<<<< HEAD
+=======
+    
+    @override
+    def hoverEnterEvent(self, event: QGraphicsSceneHoverEvent, /) -> None:
+        return super().hoverEnterEvent(event)
+>>>>>>> 1872cd6 (chore: override hoverevent)
