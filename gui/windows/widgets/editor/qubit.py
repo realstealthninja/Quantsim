4d61d6b429fdc26ea6f390665103360bbe0d6b19
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-23 08:21:51 +0530

    chore: remove merge

--- gui/windows/widgets/editor/qubit.py
+++ gui/windows/widgets/editor/qubit.py
@@ -1,7 +1,5 @@
-from typing import override
 from PySide6.QtCore import QPointF
-from PySide6.QtWidgets import QGraphicsSceneHoverEvent
-from quantsim.core.qubit import Qubit
+from quantsim.core import Qubit
 from .caditem import CADItem
 
 
@@ -9,12 +7,6 @@ class QubitCADItem(CADItem):
     qubit: Qubit
 
     def __init__(self, qubit: Qubit | None = None):
-        super().__init__("./assets/qubit.svg", None, QPointF(490/512, 260/512))
+        super().__init__("./assets/qubit.svg", None, QPointF(0, 0))
         self.qubit = qubit if qubit else Qubit()
-<<<<<<< HEAD
-=======
-    
-    @override
-    def hoverEnterEvent(self, event: QGraphicsSceneHoverEvent, /) -> None:
-        return super().hoverEnterEvent(event)
->>>>>>> 1872cd6 (chore: override hoverevent)
+        self.ouputItems = []
