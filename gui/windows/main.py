cb329e14b6fea761fa0b0effa0c90c08afef49fb
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2026-02-23 08:24:31 +0530

    feat: connect qubit and blochsphere

--- gui/windows/main.py
+++ gui/windows/main.py
@@ -1,10 +1,18 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QDockWidget, QMainWindow, QToolBar, QToolBox
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import (
+    QDockWidget,
+    QMainWindow,
+    QToolBar,
+    QToolBox,
+    QToolButton,
+)
+from quantsim.core import Qubit
 
 from .widgets.blochsphere import BlochSphere
-
-from .widgets.editor  import Editor 
+from .widgets.editor import Editor
+from .widgets.editor.editor import Tools
+from .widgets.editor.qubit import QubitCADItem
 
 
 class MainWindow(QMainWindow):
@@ -16,37 +24,63 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(1200, 800))
         self.add_widgets()
         self.add_dockables()
-        
 
+        _ = self.editor.qubititem_selected.connect(self.render_qubit)
+
+    def render_qubit(self, qubit: Qubit) -> None:
+        self.bloch.set_qubit(qubit)
 
     def add_widgets(self):
         self.editor: Editor = Editor()
-        self.toolbar: QToolBar = QToolBar()
+        self.toolbar: QToolBar = QToolBar("Tool Bar")
 
         menu = self.menuBar()
         file_menu = menu.addMenu("&File")
         file_new = QAction("&New", self)
         file_menu.addAction(file_new)
 
-
         self.view_menu = menu.addMenu("&View")
 
-        self.toolbar.addAction(QAction("&Line tool"))
+        wire_action = QAction("&Wire tool", self.toolbar)
+        _ = wire_action.triggered.connect(self.wire_selected)
+        self.toolbar.addAction(wire_action)
         self.addToolBar(self.toolbar)
         self.setCentralWidget(self.editor)
 
+    def wire_selected(self) -> None:
+        if self.editor.tool == Tools.WIRE:
+            self.editor.tool = Tools.NONE
+        else:
+            self.editor.set_tool(Tools.WIRE)
+        self.editor.cadItem = None
+
+    def qubit_selected(self, action: QAction) -> None:
+        if self.editor.tool == Tools.PLACE:
+            self.editor.tool = Tools.NONE
+        else:
+            self.editor.set_tool(Tools.PLACE)
+            self.editor.cadItem = QubitCADItem()
 
     def add_dockables(self):
         """
         adds dockable widgets to the main window
-        
+
         :param self: Description
         """
         toolboxdock = QDockWidget("Tool box")
         blochsphere = QDockWidget("Bloch Sphere")
-        self.toolbox: QToolBox  = QToolBox()
+        self.toolbox: QToolBox = QToolBox()
+        qubitbtn = QToolButton()
+        qubitbtn.setIcon(QIcon("./assets/qubit.svg"))
 
-        blochsphere.setWidget(BlochSphere())
+        qubitbtn.setIconSize(QSize(128, 128))
+        qubitaction = QAction("", qubitbtn)
+        _ = qubitaction.triggered.connect(self.qubit_selected)
+        qubitbtn.setDefaultAction(qubitaction)
+        _ = self.toolbox.addItem(qubitbtn, "core")
+
+        self.bloch = BlochSphere()
+        blochsphere.setWidget(self.bloch)
         toolboxdock.setWidget(self.toolbox)
 
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, toolboxdock)
@@ -57,11 +91,3 @@ class MainWindow(QMainWindow):
         self.view_menu.addActions([view_bloch, view_toolbox])
         self.docks["toolbox"] = toolboxdock
         self.docks["bloch"] = blochsphere
-    
-
-
-        
-        
-    
-
-
